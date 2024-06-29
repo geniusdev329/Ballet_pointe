c687be1b9401da7a88c44b4b3fd7697e09922075
geniusdev329 <geniusdev329@gmail.com>
2024-06-29 15:21:15 +0900

    fix: Fix index method in HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Blog;
+use App\Models\Maker;
+use App\Models\Notification;
+use App\Models\ProductReview;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -14,7 +18,16 @@ class HomeController extends Controller
      */
     public function index(): View
     {
-        return view('frontend.welcome');
+        $blogs = Blog::where('status', 1)->latest()->take(3)->get();
+        $notifications = Notification::where('status', 1)->latest()->take(6)->get();
+        $makers = Maker::orderBy('type')->get()
+        ->groupBy('type')
+        ->map(function ($group) {
+            return $group->pluck('name', 'id');
+        });
+
+        $product_reviews = ProductReview::where('status', 1)->latest()->take(4)->get();
+        return view('frontend.welcome', compact('blogs', 'notifications', 'makers', 'product_reviews'));
     }
 
     /**
