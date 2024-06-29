8aac7b9f61b3078207fe5412ab2d3d8bccec10d0
geniusdev329 <geniusdev329@gmail.com>
2024-06-29 15:11:53 +0900

    fix: Fix index method in HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends Controller
      */
     public function index(): View
     {
-        return view('frontend.home');
+        return view('frontend.welcome');
     }
 
     /**
